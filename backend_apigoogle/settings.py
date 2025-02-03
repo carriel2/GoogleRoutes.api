70b68c04702883a3006a69edc65849445b0ff7ed
Carriel <pcarriel05@gmail.com>
2025-02-03 11:50:29 -0300

    security-changes

--- backend_apigoogle/settings.py
+++ backend_apigoogle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1p!pcy-f2k^s%^851#+z9oqz506e7yx9nksdf&e!vu@j&=bx$^'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
